d85c424caf60273f6e2bbc8bb299d771d7032a5e
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-12-26 19:55:58 -0800

    [setup] Add `requests` to library requirements

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-   name='pyhmy',
-   version='0.11',
-   long_description=README,
-   long_description_content_type="text/markdown",
-   author='Daniel Van Der Maden',
-   author_email='daniel@harmony.one',
-   url="http://harmony.one/",
-   packages=['pyhmy'], 
-   install_requires=['pexpect'], 
+    name='pyhmy',
+    version='0.11',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Daniel Van Der Maden',
+    author_email='daniel@harmony.one',
+    url="http://harmony.one/",
+    packages=['pyhmy'],
+    install_requires=['pexpect', 'requests'],
 )
