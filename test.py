392a35915df1743c170ff81ad957d41ab5b47395
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2019-12-26 23:12:11 -0800

    [test] Update basic tests

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import pyhmy
 from pyhmy.util import download_cli
 from pyhmy.logging import ControlledLogger
+from pyhmy import cli
 
 if __name__ == "__main__":
-    # download_cli("test")
+    download_cli("test")
+    print(cli.get_version())
+    print(cli.get_binary_path())
+    print(cli.get_account_keystore_path())
+    print(cli.single_call("hmy keys list"))
     logger = ControlledLogger("test", "logs/")
-    CLI = pyhmy.HmyCLI(environment=pyhmy.get_environment())
-    z = pyhmy.get_goversion()
+    print(pyhmy.get_goversion())
+    logger.info("test info")
+    logger.debug("test debug")
+    logger.error("test error")
+    logger.warning("test warning")
+    logger.write()
 
