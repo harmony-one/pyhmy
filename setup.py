fe3f7cf28a204e8a723fff8f072e0e5474b192bd
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-01 15:35:51 -0800

    [setup] Add licence and tags for pypi

--- setup.py
+++ setup.py
@@ -7,12 +7,40 @@ README = (HERE / "README.md").read_text()
 setup(
     name='pyhmy',
     use_incremental=True,
-    setup_requires=['incremental', 'click', 'twisted'],
+    license='MIT',
+    description="A library for interacting and working the Harmony blockchain and related codebases.",
     long_description=README,
     long_description_content_type="text/markdown",
     author='Daniel Van Der Maden',
     author_email='daniel@harmony.one',
     url="http://harmony.one/",
     packages=['pyhmy'],
-    install_requires=['pexpect', 'requests', 'pytest', 'pytest-ordering', 'incremental', 'click', 'twisted'],
+    keywords=['Harmony', 'blockchain', 'protocol'],
+    install_requires=[
+        'pexpect',
+        'requests',
+        'pytest',
+        'pytest-ordering',
+        'incremental',
+        'click',
+        'twisted'
+    ],
+    setup_requires=[
+        'incremental',
+        'click',
+        'twisted'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
+
