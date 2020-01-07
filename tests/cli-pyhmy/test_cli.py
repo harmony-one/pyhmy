cc13ebcee9de0633ea8ba50fff99305b1005b305
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-07 13:13:09 -0800

    [test] Update tests for new cli download function

--- tests/cli-pyhmy/test_cli.py
+++ tests/cli-pyhmy/test_cli.py
@@ -14,12 +14,16 @@ BINARY_FILE_PATH = f"{TEMP_DIR}/bin/cli_test_binary"
 def setup():
     shutil.rmtree(TEMP_DIR, ignore_errors=True)
     os.makedirs(TEMP_DIR, exist_ok=True)
-    from pyhmy.util import download_cli
-    download_cli(BINARY_FILE_PATH, replace=False, verbose=False)
-    assert os.path.exists(BINARY_FILE_PATH)
 
 
 @pytest.mark.run(order=0)
+def test_download_cli():
+    env = cli.download(BINARY_FILE_PATH, replace=False, verbose=False)
+    cli.environment.update(env)
+    assert os.path.exists(BINARY_FILE_PATH)
+
+
+@pytest.mark.run(order=1)
 def test_is_valid():
     bad_file_path = os.path.realpath(f"{TEMP_DIR}/test_is_valid/bad_hmy")
     shutil.rmtree(Path(bad_file_path).parent, ignore_errors=True)
@@ -31,7 +35,7 @@ def test_is_valid():
     assert not cli.is_valid_binary(bad_file_path)
 
 
-@pytest.mark.run(order=1)
+@pytest.mark.run(order=2)
 def test_bad_bin_set():
     bad_file_path = os.path.realpath(f"{TEMP_DIR}/test_bad_bin_set/hmy")
     shutil.rmtree(Path(bad_file_path).parent, ignore_errors=True)
@@ -42,7 +46,7 @@ def test_bad_bin_set():
     assert cli.get_binary_path() != bad_file_path
 
 
-@pytest.mark.run(order=2)
+@pytest.mark.run(order=3)
 def test_bin_set():
     cli.set_binary(BINARY_FILE_PATH)
     cli_binary_path = cli.get_binary_path()
