b27315b0f7c16a938a9e9556fa8b5a623b459977
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-04-08 02:17:48 -0700

    [version] Update version to release

--- pyhmy/_version.py
+++ pyhmy/_version.py
@@ -7,5 +7,5 @@ Provides pyhmy version information.
 
 from incremental import Version
 
-__version__ = Version('pyhmy', 20, 1, 25)
+__version__ = Version('pyhmy', 20, 1, 27)
 __all__ = ["__version__"]
