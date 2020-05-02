8d136a7bb88c96667cd5223807ee645e6c0ac432
Janet Liang <janet@harmony.one>
2020-05-02 12:35:32 -0700

    [blockchain] Added more RPCs, organized file based on RPC type (network or block)

--- pyhmy/blockchain.py
+++ pyhmy/blockchain.py
@@ -7,6 +7,9 @@ from .request import (
 )
 
 
+################
+# Network RPCs #
+################
 def get_node_metadata(endpoint = default_endpoint, timeout = default_timeout):
     """
     Get config for the node
@@ -64,44 +67,6 @@ def get_leader_address(endpoint = default_endpoint, timeout = default_timeout):
     return rpc_request('hmy_getLeader', endpoint, [], timeout)['result']
 
 
-def get_latest_header(endpoint = default_endpoint, timeout = default_timeout):
-    """
-    Get block header of latest block
-
-    Parameters
-    ----------
-    endpoint: :obj:`str`, optional
-        Endpoint to send request to
-    timeout: :obj:`int`, optional
-        Timeout in seconds
-
-    Returns
-    -------
-    dict
-        # TODO: Add link to reference RPC documentation
-    """
-    return rpc_request('hmy_latestHeader', endpoint, [], timeout)['result']
-
-
-def get_latest_headers(endpoint = default_endpoint, timeout = default_timeout):
-    """
-    Get block header of latest block for beacon chain & shard chain
-
-    Parameters
-    ----------
-    endpoint: :obj:`str`, optional
-        Endpoint to send request to
-    timeout: :obj:`int`, optional
-        Timeout in seconds
-
-    Returns
-    -------
-    dict
-        # TODO: Add link to reference RPC documentation
-    """
-    return rpc_request('hmy_getLatestChainHeaders', endpoint, [], timeout)['result']
-
-
 def get_block_number(endpoint = default_endpoint, timeout = default_timeout):
     """
     Get current block number
@@ -138,6 +103,85 @@ def get_current_epoch(endpoint = default_endpoint, timeout = default_timeout):
         Current epoch number
     """
     return int(rpc_request('hmy_getEpoch', endpoint, [], timeout)['result'])
+
+
+def get_gas_price(endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get network gas price
+
+    Parameters
+    ----------
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    int
+        Network gas price
+    """
+    return int(rpc_request('hmy_gasPrice', endpoint, [], timeout)['result'])
+
+
+def get_num_peers(endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get number of peers
+
+    Parameters
+    ----------
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    int
+        Count of connected peers
+    """
+    return int(rpc_request('net_peerCount', endpoint, [], timeout)['result'])
+
+
+##############
+# Block RPCs #
+##############
+def get_latest_header(endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get block header of latest block
+
+    Parameters
+    ----------
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    dict
+        # TODO: Add link to reference RPC documentation
+    """
+    return rpc_request('hmy_latestHeader', endpoint, [], timeout)['result']
+
+
+def get_latest_headers(endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get block header of latest block for beacon chain & shard chain
+
+    Parameters
+    ----------
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    dict
+        # TODO: Add link to reference RPC documentation
+    """
+    return rpc_request('hmy_getLatestChainHeaders', endpoint, [], timeout)['result']
 
 
 def get_block_by_hash(block_hash, endpoint = default_endpoint, include_full_tx = False, timeout = default_timeout):
@@ -192,6 +236,60 @@ def get_block_by_number(block_num, endpoint = default_endpoint, include_full_tx 
         include_full_tx
     ]
     return rpc_request('hmy_getBlockByNumber', endpoint, params, timeout)['result']
+
+
+def get_block_transaction_count_by_hash(block_hash, endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get block by hash
+
+    Parameters
+    ----------
+    block_hash: str
+        Block hash to get transaction count
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    include_full_tx: :obj:`bool`, optional
+        Include list of full transactions data for each block
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    int
+        Number of transactions in the block
+    """
+    params = [
+        block_hash,
+        include_full_tx
+    ]
+    return int(rpc_request('hmy_getBlockByHash', endpoint, params, timeout)['result'])
+
+
+def get_block_transaction_count_by_number(block_num, endpoint = default_endpoint, timeout = default_timeout):
+    """
+    Get block by number
+
+    Parameters
+    ----------
+    block_num: int
+        Block number to get transaction count
+    endpoint: :obj:`str`, optional
+        Endpoint to send request to
+    include_full_tx: :obj:`bool`, optional
+        Include list of full transactions data for each block
+    timeout: :obj:`int`, optional
+        Timeout in seconds
+
+    Returns
+    -------
+    int
+        Number of transactions in the block
+    """
+    params = [
+        str(hex(block_num)),
+        include_full_tx
+    ]
+    return int(rpc_request('hmy_getBlockByNumber', endpoint, params, timeout)['result'])
 
 
 def get_blocks(start_block, end_block, endpoint = default_endpoint, include_full_tx = False,
