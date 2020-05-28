e7664217dc703d2dab47f0aae54858acdf0063ed
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-05-28 06:29:29 -0700

    bump version

--- pyhmy/_version.py
+++ pyhmy/_version.py
@@ -7,5 +7,5 @@ Provides pyhmy version information.
 
 from incremental import Version
 
-__version__ = Version('pyhmy', 20, 5, 6)
+__version__ = Version('pyhmy', 20, 5, 7)
 __all__ = ["__version__"]
