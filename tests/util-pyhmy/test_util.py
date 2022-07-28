25a058d5ddb384ee7a8fb00ec2089fb07832eecb
mikefreemanwd <mikefreemanwd@gmail.com>
2022-07-28 11:22:59 -0400

    [feature]: Added utility function to convert from hex to one (other way from one to hex). Added vim .swp files to gitignore.

--- tests/util-pyhmy/test_util.py
+++ tests/util-pyhmy/test_util.py
@@ -47,6 +47,11 @@ def test_convert_one_to_hex():
     assert util.convert_one_to_hex('0xebcd16e8c1d8f493ba04e99a56474122d81a9c58') == '0xeBCD16e8c1D8f493bA04E99a56474122D81A9c58'
     assert util.convert_one_to_hex('one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9') == '0xeBCD16e8c1D8f493bA04E99a56474122D81A9c58'
 
+def test_convert_hex_to_one():
+    assert util.convert_hex_to_one('0xebcd16e8c1d8f493ba04e99a56474122d81a9c58') == 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9'
+    assert util.convert_hex_to_one('one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9') == 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9'
+    
+
 def test_get_bls_build_variables():
     assert isinstance(util.get_bls_build_variables(), dict)
 
